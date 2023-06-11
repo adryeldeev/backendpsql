9663dc6581aad029232fb0204af67931883a6b2d
adryel martins <adryeldev@hotmail.com>
2023-06-10 23:37:59 -0300

    two deploy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { router } from "./routes.js";
 
     
 const app = express();
+const port = process.env.PORT || 8081
 
 app.use(express.json())
 app.use(cors())
@@ -13,4 +14,4 @@ app.use(router)
 
 
 
-app.listen(8081, () => console.log("Servidor rodando na porta 8081"));
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
